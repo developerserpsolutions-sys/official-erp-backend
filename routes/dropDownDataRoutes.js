d8308b3c14e01c4ac5d05ca53b8e0e90aaa6cc6e
anurag-int <ag6526803@gmail.com>
2025-09-30 01:07:14 +0530

    feature: old files delete

--- routes/dropDownDataRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getDropDownData } from "../controllers/getDropdownData.js";
-
-const router = express.Router();
-
-router.get("/:businessObject", getDropDownData);
-
-export default router;
